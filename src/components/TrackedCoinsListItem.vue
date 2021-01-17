5a5d9a82fc6ebf65db78ff11b318925c6e0239ba
m6ttw <matthewrhyswilliams@live.com>
2021-01-17 14:54:33 +0000

    formatting

--- src/components/TrackedCoinsListItem.vue
+++ src/components/TrackedCoinsListItem.vue
@@ -1,5 +1,12 @@
 <template>
-  <li>{{ coin.name }}</li>
+  <li><h4>({{coin.symbol}}) {{ coin.name }}<br></h4>
+    <p>Price: ${{coin.price_usd}}</p>
+    <p>Change (%):
+      <br>1h: {{coin.percent_change_1h}}
+      <br>24h: {{coin.percent_change_24h}}
+      <br>7 days: {{coin.percent_change_7d}}
+    </p>
+    <p>Market Cap: ${{coin.market_cap_usd}}</p></li>
 </template>
 
 <script>
